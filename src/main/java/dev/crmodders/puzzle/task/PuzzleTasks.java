7412ba7dd71703c4e65f07faa761b9d5e2bbe704
Mr-Zombii <thezombiigames@gmail.com>
2024-06-05 16:26:29 -0500

    Fix tasks

--- src/main/java/dev/crmodders/puzzle/task/PuzzleTasks.java
+++ src/main/java/dev/crmodders/puzzle/task/PuzzleTasks.java
@@ -1,6 +1,7 @@
 package dev.crmodders.puzzle.task;
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
+import dev.crmodders.puzzle.Constants;
 import dev.crmodders.puzzle.CosmicPuzzleInfo;
 import dev.crmodders.puzzle.extention.PuzzleGradleExtension;
 import dev.crmodders.puzzle.task.tasks.RunClientTask;
@@ -12,6 +13,7 @@ import org.gradle.api.initialization.dsl.ScriptHandler;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.bundling.Jar;
+import org.gradle.language.jvm.tasks.ProcessResources;
 
 import javax.inject.Inject;
 import java.util.Collections;
@@ -36,30 +38,31 @@ public abstract class PuzzleTasks implements Runnable {
 
         // Jar Creation
         getTasks().register("buildSlimJar", Jar.class, t -> {
-            t.setGroup(CosmicPuzzleInfo.GROUP);
-            t.from(getProject().task("processResources").getOutputs().getFiles());
+            t.setGroup(Constants.Tasks.GROUP);
+            t.from(getProject().getTasks().getByName("processResources").getOutputs().getFiles());
             t.from(getProject().getExtensions().getByType(SourceSetContainer.class).getByName("main").getAllJava().getClassesDirectory());
 
             t.setDescription("Builds a jar with no bundled dependencies");
         });
 
         getTasks().register("buildBundleJar", ShadowJar.class, t -> {
-            t.setGroup(CosmicPuzzleInfo.GROUP);
+            t.setGroup(Constants.Tasks.GROUP);
             t.setConfigurations(Collections.singletonList(getProject().getConfigurations().getByName("bundle")));
-            t.from(getProject().task("processResources").getOutputs().getFiles());
+            t.from(getProject().getTasks().getByName("processResources").getOutputs().getFiles());
             t.from(getProject().getExtensions().getByType(SourceSetContainer.class).getByName("main").getAllJava().getClassesDirectory());
 
             t.setDescription("Builds a jar with all of the dependencies bundled");
         });
 
         getTasks().register("buildSourcesJar", Jar.class, t -> {
-            t.setGroup(CosmicPuzzleInfo.GROUP);
+            t.setGroup(Constants.Tasks.GROUP);
             t.from(getProject().getExtensions().getByType(SourceSetContainer.class).getByName("main").getAllJava().getSourceDirectories());
 
             t.setDescription("Builds a jar with no bundled dependencies");
         });
 
         getTasks().register("buildAllJars", t -> {
+            t.setGroup(Constants.Tasks.GROUP);
             t.dependsOn("buildSlimJar");
             t.dependsOn("buildBundleJar");
             t.dependsOn("buildSourcesJar");
