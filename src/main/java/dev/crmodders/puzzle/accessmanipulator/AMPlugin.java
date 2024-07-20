e441c4b2b704082ee7b1c94a6193e6b314835c6a
Mr Zombii <thezombiigames@gmail.com>
2024-07-19 19:51:37 -0500

    sync transformation test-1

--- src/main/java/dev/crmodders/puzzle/accessmanipulator/AMPlugin.java
+++ src/main/java/dev/crmodders/puzzle/accessmanipulator/AMPlugin.java
@@ -24,7 +24,43 @@ public abstract class AMPlugin implements Runnable {
     public void run() {
         Attribute<Boolean> manipulated = Attribute.of("manipulated", Boolean.class);
 
-        getProject().
+        getProject().sync(snc -> {
+            PuzzleGradleExtension extension = CosmicPuzzlePlugin.EXTENTION;
+
+            var proj = getProject();
+            proj.getDependencies().getAttributesSchema().attribute(manipulated);
+            proj.getDependencies().getArtifactTypes().getByName("jar", artifact -> {
+                artifact.getAttributes().attribute(manipulated, false);
+            });
+
+
+            proj.getDependencies().registerTransform(AMClassTransformer.class, param -> {
+                param.getFrom().attribute(manipulated, false);
+                param.getTo().attribute(manipulated, true);
+
+                ListProperty<File> manipulators = this.getProject().getObjects().listProperty(File.class);
+                try {
+                    param.parameters(parameters -> {
+                        if (extension.getForgeAccessTransformerPath().isPresent())
+                            manipulators.add(extension.getForgeAccessTransformerPath().get().getAsFile());
+                        if (extension.getFabricAccessWidenerPath().isPresent())
+                            manipulators.add(extension.getFabricAccessWidenerPath().get().getAsFile());
+                        if (extension.getAccessManipulatorPath().isPresent())
+                            manipulators.add(extension.getAccessManipulatorPath().get().getAsFile());
+
+                        parameters.getAccessManipulatorPaths().set(manipulators);
+                    });
+                } catch (Exception e) {
+                    LOGGER.error("Could not read AM file(s)", e);
+                    param.getParameters().getAccessManipulatorPaths().set(manipulators);
+                }
+            });
+
+            getConfigurations().all(config -> {
+                if (config.isCanBeResolved())
+                    config.getAttributes().attribute(manipulated, true);
+            });
+        });
 
         getProject().afterEvaluate(proj -> {
             PuzzleGradleExtension extension = CosmicPuzzlePlugin.EXTENTION;
